4947e203c020eca77ef2594dfe3e0f33795988a7
ngactive99 <ngactive99@gmail.com>
2022-08-16 12:42:04 +0530

    created service and controller for post and get all employees

--- src/main/java/com/emp/service/EmployeeServiceImpl.java
+++ src/main/java/com/emp/service/EmployeeServiceImpl.java
@@ -1,5 +1,7 @@
 package com.emp.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,11 @@ public class EmployeeServiceImpl implements IEmployeeService {
 	public Integer saveEmployee(Employee employee) {
 		Employee savedEmployee = employeeRepository.save(employee);
 		return savedEmployee.getId();
+	}
+
+	@Override
+	public List<Employee> getAllEmployees() {
+		return employeeRepository.findAll();
 	}
 
 }
