177dc172061226e1359ed1a90426057a6ef15524
ngactive99 <ngactive99@gmail.com>
2022-08-17 11:52:59 +0530

    resource not found exception and update employee method implemented

--- src/main/java/com/emp/controller/EmployeeController.java
+++ src/main/java/com/emp/controller/EmployeeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,46 +20,48 @@ import com.emp.service.IEmployeeService;
 
 @RestController
 public class EmployeeController {
-	
+
 	@Autowired
 	IEmployeeService employeeService;
-	
+
 	@RequestMapping("/hello")
 	public String hello() {
 		return "Hello world";
 	}
-	
-	@PostMapping("/employee") 
+
+	@PostMapping("/employee")
 	Integer createEmployee(@RequestBody Employee employee) {
 		Integer id = employeeService.saveEmployee(employee);
 		System.out.println(id);
 		return id;
 	}
-	
+
 	@GetMapping("/allemployees")
 	public List<Employee> getEmployees() {
 		return employeeService.getAllEmployees();
 	}
-	
-	@GetMapping("/getemployee/{id}") 
-		public Optional<Employee> getEmployee(@PathVariable Integer id) {
-			Optional<Employee> employee = employeeService.getEmployee(id);
-			return employee;
-		}
+
+	@GetMapping("/getemployee/{id}")
+	public Optional<Employee> getEmployee(@PathVariable Integer id) {
+		Optional<Employee> employee = employeeService.getEmployee(id);
+		return employee;
+	}
+
 	@DeleteMapping("/employee/{id}")
 	public ResponseEntity<Employee> deleteEmployee(@PathVariable Integer id) {
 		System.out.println(id);
-		ResponseEntity<Employee> responseEntity = new ResponseEntity<>(HttpStatus.OK) ;
+		ResponseEntity<Employee> responseEntity = new ResponseEntity<>(HttpStatus.OK);
 		try {
 			employeeService.deleteEmployee(id);
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			responseEntity = new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		return responseEntity;
-		
-	}
-	}
 
-
+	}
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable("id") Integer id, @RequestBody Employee employee ) {
+		return new ResponseEntity<Employee>(employeeService.updateEmployee(employee, id), HttpStatus.OK);
+	}
+}
--- src/main/java/com/emp/service/EmployeeServiceImpl.java
+++ src/main/java/com/emp/service/EmployeeServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.emp.exception.ResourceNotFoundException;
 import com.emp.model.Employee;
 
 @Service
@@ -34,6 +35,20 @@ public class EmployeeServiceImpl implements IEmployeeService {
 	public void deleteEmployee(Integer id) {
 			employeeRepository.deleteById(id);
 		
+	}
+
+	@Override
+	public Employee updateEmployee(Employee employee, Integer id) {
+		// we will check weather the employee exists with the given id or not
+		Employee existingEmployee = employeeRepository.findById(id).orElseThrow(
+				()-> new ResourceNotFoundException("Employee", "id", id));
+		existingEmployee.setFirstName(employee.getFirstName());
+		existingEmployee.setLastName(employee.getLastName());
+		existingEmployee.setEmail(employee.getEmail());
+		
+		// we will update the value and then save the updated value
+		employeeRepository.save(existingEmployee);
+		return existingEmployee;
 	}
 	
 	
